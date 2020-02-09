1b7c44c4c6674f5066fa0e9a90a3c9776677637d
trevor <trevku155@gmail.com>
2020-02-09 13:39:16 -0500

    forgot app.py file in flask_redis

--- flask_redis/app.py
+++ flask_redis/app.py
@@ -1,11 +1,9 @@
 import time
-
 import redis
 from flask import Flask
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
-
 
 def get_hit_count():
     retries = 5
@@ -18,8 +16,8 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 
-
 @app.route('/')
 def hello():
     count = get_hit_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
+
